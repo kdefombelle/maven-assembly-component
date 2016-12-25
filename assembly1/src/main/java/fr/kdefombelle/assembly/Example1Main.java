e16ef6a811d58c56c82a6cc2258de8896bc83bca
Karim de Fombelle <karim.defombelle@yahoo.fr>
2016-12-25 21:58:19 +0800

    second module leveraging assembly component for a better illustration

---
+++ assembly1/src/main/java/fr/kdefombelle/assembly/Example1Main.java
@@ -0,0 +1,26 @@
+package fr.kdefombelle.assembly;
+
+import java.util.ResourceBundle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Dummy main application
+ * 
+ * @author kdefombelle
+ */
+public class Example1Main 
+{
+	
+    private static Logger LOGGER = LoggerFactory.getLogger(Example1Main.class);
+    
+    public static void main( String[] args )
+    {
+    	LOGGER.info( "Let's print the properties from a file common.properties in the classpath" );
+    	ResourceBundle bundle = ResourceBundle.getBundle("common");
+    	bundle.keySet().stream().sorted().forEach(k -> LOGGER.info("{}:{}", k, bundle.getString(k)));
+    	LOGGER.info( "It is not this maion class which is interesting in this example" );
+    	LOGGER.info( "It is the fact the assembly in this project is based on a common descriptor in assembly-component module" );
+    }
+}
